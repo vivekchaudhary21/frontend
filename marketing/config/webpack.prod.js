44271213873f2622b893d15f48f55c69922fbfbc
Vivek Chaudhary <vivchaud1@WKMZTBB60083.global.publicisgroupe.net>
2022-06-26 09:02:12 +0530

    code refactor

--- marketing/config/webpack.prod.js
+++ marketing/config/webpack.prod.js
@@ -1,7 +1,7 @@
 const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const commonConfig = require('./webpack.common');
 const packageJSON = require('../package.json');
+const commonConfig = require('./webpack.common');
 
 const prodConfig = {
   mode: 'production',
