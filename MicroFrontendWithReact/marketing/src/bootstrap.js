236cb0374d14406f39521060f0afd768d2e385b4
Vivek Chaudhary <vivchaud1@WKMZTBB60083.global.publicisgroupe.net>
2022-07-01 11:22:14 +0530

    added multi tier navigation

--- MicroFrontendWithReact/marketing/src/bootstrap.js
+++ MicroFrontendWithReact/marketing/src/bootstrap.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate: ({ pathname: nextPathname }) => {
+      if (history.location.pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 if (process.env.NODE_ENV === 'development') {
   const el = document.querySelector('#marketing-root');
   if (el) {
-    mount(el);
+    mount(el, { defaultHistory: createBrowserHistory() });
   }
 }
 
