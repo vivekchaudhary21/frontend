24c7fc4517c6831ba8c699f3eaffc6612940e34f
Vivek Chaudhary <vivchaud1@WKMZTBB60083.global.publicisgroupe.net>
2022-07-03 15:55:32 +0530

    added builder pattern example

---
+++ Practical Design Pattern/Creational Design Pattern/Builder.js
@@ -0,0 +1,41 @@
+/**
+ * Builder provides an API for constructing an object step by step
+ */
+
+class Tag {
+  constructor(name, text) {
+    this.name = name;
+    this.text = text;
+  }
+
+  toString() {
+    return `  <${this.name}>${this.text}</${this.name}>\n`;
+  }
+}
+
+class HTMLBuilder {
+  constructor(rootName) {
+    this.root = new Tag(rootName);
+    this.root.children = [];
+  }
+
+  addChild(childname, childtext) {
+    let child = new Tag(childname, childtext);
+    this.root.children.push(child);
+  }
+
+  toString() {
+    return `<${this.root.name}>\n${this.root.children.join('')}</${
+      this.root.name
+    }>`;
+  }
+}
+
+const words = ['js', 'react', 'webpack', 'gql'];
+
+const ul = new HTMLBuilder('ul');
+for (let word of words) {
+  ul.addChild('li', word);
+}
+
+console.log(ul.toString());
