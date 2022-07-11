8cdc2f6342536507666e8f99f355a7021e5ae0af
Vivek Chaudhary <vivchaud1@WKMZTBB60083.global.publicisgroupe.net>
2022-07-11 09:01:25 +0530

    Finished memory game

--- React Projects/Memory Game/src/index.js
+++ React Projects/Memory Game/src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const cardImages = [
+  { src: '/img/helmet-1.png', matched: false },
+  { src: '/img/potion-1.png', matched: false },
+  { src: '/img/ring-1.png', matched: false },
+  { src: '/img/scroll-1.png', matched: false },
+  { src: '/img/shield-1.png', matched: false },
+  { src: '/img/sword-1.png', matched: false },
+];
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App cardImages={cardImages} />
   </React.StrictMode>,
   document.getElementById('root')
 );
