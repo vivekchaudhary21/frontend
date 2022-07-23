5977f6401be52fe0d2d0cbf2b28305dd1cc42e3f
Vivek Chaudhary <vivchaud1@WKMZTBB60083.global.publicisgroupe.net>
2022-07-23 10:38:00 +0530

    crefacoring

--- JavaScript/basics/array_methods.js
+++
@@ -1,9 +0,0 @@
-/**
- * New Array.at method
- * Benefit: also support negative indexes
- */
-
-const arr = ['Vihaan', 'Vivek'];
-
-arr.at(-1); // Vivek
-arr.at(-2); // Vihaan
--- JavaScript/practical_design_pattern/behavioral_design_pattern/observer.js
+++
@@ -1,4 +0,0 @@
-/**
- * Used when there is one-to-many relationship between objects such as if one object is modified,
- * its depenedent objects are to be notified automatically
- */
