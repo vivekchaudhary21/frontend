9f08628058f4502e8740244bfa57e716e236d691
vivekchaudhary21 <vivek_chaudhry21@yahoo.co.in>
2022-09-28 21:51:44 +0530

    Added nodemon in ts basics and completed dates and times in js basics

--- javascript/basics/dates_and_times.js
+++ javascript/basics/dates_and_times.js
@@ -25,8 +25,8 @@ let century = new Date(2100, 0, 1, 2, 3, 4, 5); // 2099-12-31T20:33:04.005Z
 
 // If you want to specify a date and time in UTC (Universal Coordinated Time, aka GMT), then you can use the Date.UTC()
 
-let centuryUTC = new Date(Date.UTC(2100, 0, 1)); // 2100-01-01T00:00:00.000Z
-let centuryUTCWithTime = new Date(Date.UTC(2100, 0, 1, 3, 4, 5)); // 2100-01-01T03:04:05.000Z
+let century_UTC = new Date(Date.UTC(2100, 0, 1)); // 2100-01-01T00:00:00.000Z
+let century_UTC_with_time = new Date(Date.UTC(2100, 0, 1, 3, 4, 5)); // 2100-01-01T03:04:05.000Z
 
 // If you want to display a date in UTC, you should explicitly convert it to a string with toUTCString() or toISOString().
 
@@ -46,3 +46,93 @@ d.setFullYear(d.getFullYear() + 1); // Increment the year: 2023-09-26T12:00:54.0
 
 /*****************************************************************************/
 /********************************** TIMESTAMPS *******************************/
+/*****************************************************************************/
+
+/**
+ * JavaScript represents dates internally as integers that specify the number of milliseconds
+ * since (or before) midnight on January 1, 1970, UTC time.
+ *
+ * For any Date object, the getTime() method returns this internal value, and the set Time() method sets it.
+ */
+
+d = new Date();
+d.getTime(); // 1664377899403
+d.setTime(d.getTime() + 30000); // added 30 seconds => 166437808
+
+/**
+ * The static Date.now() method returns the current time as a timestamp
+ */
+
+let start_time = Date.now();
+// reticulateSplines(); // Do some time-consuming operation
+let end_time = Date.now();
+let time_diff = end_time - start_time;
+
+/**
+ * The performance object is part of a larger Performance API that is not defined by the ECMAScript standard.
+ * But it is implemented by web browsers and by Node. 
+ * 
+ * In order to use the performance object in Node, you must import it with:
+
+ */
+
+const { performance } = require('perf_hooks');
+
+start_time = performance.now();
+let i = 0;
+while (i < 1000000) {
+  i++;
+}
+end_time = performance.now();
+time_diff = end_time - start_time; // almost 4 ms
+
+/*****************************************************************************/
+/******************************* DATE ARITHMATIC *****************************/
+
+/**
+ * Date objects can be compared with JavaScript’s standard <, <=, >, and >= comparison operators.
+ * And you can subtract one Date object from another to determine the number of milliseconds between the two dates.
+ * This works because the Date class defines a valueOf() method that returns a timestamp.
+ *
+ * To do date arithmetic involving days, months, and years, you can use setDate(), setMonth(), and setYear().
+ */
+
+d = new Date(); // 2022-09-28T15:40:21.343Z
+console.log(d);
+new Date(d.setMonth(d.getMonth() + 3, d.getDate() + 14)); //2023-01-11T15:40:21.343Z
+
+/*****************************************************************************/
+/****************** FORMATTING AND PARSING DATE STRINGS **********************/
+
+d = new Date(2020, 0, 1, 17, 10, 30); // 2020-01-01T11:40:30.000Z
+d.toString(); // => Wed Jan 01 2020 17:10:30 GMT+0530 (India Standard Time)
+d.toUTCString(); // => Wed, 01 Jan 2020 11:40:30 GMT
+d.toLocaleDateString(); // => 01/01/2020
+d.toLocaleTimeString(); // => 17:10:30
+d.toISOString(); // => 2020-01-01T11:40:30.000Z
+
+/**
+ * toString(): This method uses the local time zone but does not format the date and time in a locale-aware way.
+ *
+ * toUTCString(): This method uses the UTC time zone but does not format the date in a localeaware way.
+ *
+ * toISOString():
+ * This method prints the date and time in the standard year-month-day
+ * hours:minutes:seconds.ms format of the ISO-8601 standard.
+ * The letter “T” separates the date portion of the output from the time portion of the output.
+ * The timeis expressed in UTC, and this is indicated with the letter “Z” as the last letter of the output.
+ *
+ * toLocaleString(): This method uses the local time zone and a format that is appropriate for the user’s locale.
+ *
+ * toDateString():
+ * This method formats only the date portion of the Date and omits the time.
+ * It usesthe local time zone and does not do locale-appropriate formatting.
+ *
+ * toLocaleDateString(): This method formats only the date. It uses the local time zone and a localeappropriate date format.
+ *
+ * toTimeString():
+ * This method formats only the time and omits the date.
+ * It uses the local time zone but does not format the time in a locale-aware way.
+ *
+ * toLocaleTimeString(): This method formats the time in a locale-aware way and uses the local time zone.
+ */
