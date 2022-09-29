1779ca52aa1aa3443f3361e3d5aa35996f684d49
vivekchaudhary21 <vivek_chaudhry21@yahoo.co.in>
2022-09-29 16:16:55 +0530

    Code added

---
+++ typescript/basics/basic-types.ts
@@ -0,0 +1,33 @@
+const log = console.log;
+
+export const myValue: string = 'foo';
+export let myVariable: number = 1;
+myVariable += 1;
+
+export let myBoolean: boolean = true;
+
+export let myRETest: RegExp = /foo/;
+
+export let myValues: number[] = [1, 2, 3];
+export let myValues2: Array<string> = ['a'];
+
+export interface Person {
+  first: string;
+  last: string;
+  age?: number;
+}
+
+export const myPerson: Person = {
+  first: 'jack',
+  last: 'herrington',
+};
+
+export const data: Record<number, string> = {
+  10: 'megan',
+  20: 'lori',
+};
+
+data[5] = 'joe';
+
+myValues.forEach((a) => a * 2);
+export const multipliedValues = myValues.map((a) => a * 10);
