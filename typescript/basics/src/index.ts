6607223cd05c82548d9649042a22cd149fe5f005
Vivek Chaudhary <vivek_chaudhry21@yahoo.co.in>
2022-10-08 22:44:51 +0530

    Tuples types in ts basics

--- typescript/basics/src/index.ts
+++ typescript/basics/src/index.ts
@@ -1,5 +1,6 @@
 import * as basicTypes from './basic-types'
 import * as functionTypes from './functions'
+import * as tuplesTypes from './tuples'
 import houses from './houses.json'
 
 basicTypes.myRETest // /foo/
@@ -17,3 +18,8 @@ functionTypes.naturalNumbersSum(5)
 functionTypes.parseCoordinate({ x: 2, y: 2 })
 functionTypes.parseCoordinate(3, 2)
 functionTypes.parseCoordinate('x:12,y:22')
+tuplesTypes.add3dCoordinate([1, 2, 3], [1, 2, 3])
+const [stringGetter, stringSetter] = tuplesTypes.strindState('first')
+stringGetter() // first
+stringSetter('second')
+stringGetter() // second
---
+++ typescript/basics/src/tuples.ts
@@ -0,0 +1,23 @@
+/***************************************************************************/
+/*********************************** Tuples ********************************/
+/***************************************************************************/
+
+export type threeDCordinates = [x: number, y: number, z: number]
+
+export function add3dCoordinate(c1: threeDCordinates, c2: threeDCordinates): threeDCordinates {
+  const result = []
+  for (let i = 0; i < c1.length; i++) {
+    result[i] = c1[i] + c2[i]
+  }
+  return result as threeDCordinates
+}
+
+export function strindState(intialValue: string): [() => string, (v: string) => void] {
+  let str: string = intialValue
+  return [
+    () => str,
+    (v: string) => {
+      str = v
+    }
+  ]
+}
