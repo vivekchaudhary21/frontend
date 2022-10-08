3953666d1b9cd3557589aa89b2170b99419fa295
Vivek Chaudhary <vivek_chaudhry21@yahoo.co.in>
2022-10-08 12:08:08 +0530

    Added function types in typescript

--- typescript/basics/functions.ts
+++ typescript/basics/functions.ts
@@ -1,3 +1,9 @@
+/***************************************************************************/
+/********************************* Functions *******************************/
+/***************************************************************************/
+
+import { v4 as uuidv4 } from 'uuid'
+
 function addNumbers(a: number, b: number): number {
   return a + b
 }
@@ -25,3 +31,82 @@ export function getName(user: { first: string; last: string }): string {
 /***************************************************************************/
 /************************* Functions with Functions ************************/
 /***************************************************************************/
+
+export function printToFile(text: string, callback: (text: string) => void): void {
+  callback(text)
+}
+
+export function mutate(array: number[], mutateFunc: (v: number) => number) {
+  return array.map(mutateFunc)
+}
+
+export function closureFunc(param1: string): (p: string) => void {
+  return function (param2: string): string {
+    return `This is a closure function passed ${param1} and ${param2} as arguments`
+  }
+}
+
+export function naturalNumbersSum(n: number, sum: number = 0): number {
+  if (n > 0) {
+    sum += n
+    return naturalNumbersSum(n - 1, sum)
+  }
+  return sum
+}
+
+/***************************************************************************/
+/**************************** Function Overloading *************************/
+/***************************************************************************/
+
+interface Coordinate {
+  x: number
+  y: number
+}
+
+export function parseCoordinate(obj: Coordinate): Coordinate
+export function parseCoordinate(obj: string): Coordinate
+export function parseCoordinate(x: number, y: number): Coordinate
+
+export function parseCoordinate(input: Coordinate | number | string, input2?: number): Coordinate {
+  let coords: Coordinate = { x: 0, y: 0 }
+
+  if (input instanceof Object) {
+    coords = { ...input }
+  } else if (typeof input === 'string') {
+    input.split(',').forEach(str => {
+      const [key, value] = str.split(':')
+      coords[key as 'x' | 'y'] = parseInt(value, 10)
+    })
+  } else {
+    coords = { x: input, y: input2 || 0 }
+  }
+  return coords
+}
+
+/***************************************************************************/
+/***************************** Challenge 1 *********************************/
+/***************************************************************************/
+
+interface House {
+  name: string
+  planets: string | string[]
+}
+
+interface HouseWithID {
+  id: string
+  name: string
+  planets: string | string[]
+}
+
+export function findHouses(houses: string): HouseWithID[]
+export function findHouses(houses: string, filter: (house: House) => boolean): HouseWithID[]
+export function findHouses(houses: House[]): HouseWithID[]
+export function findHouses(houses: House[], filter: (house: House) => boolean): HouseWithID[]
+
+export function findHouses(input: string | House[], filter?: (house: House) => boolean): HouseWithID[] {
+  const houses = typeof input === 'string' ? JSON.parse(input) : input
+  return (filter ? houses.filter(filter) : houses).map((house: House) => ({
+    id: uuidv4(),
+    ...house
+  }))
+}
--- typescript/basics/index.ts
+++ typescript/basics/index.ts
@@ -1,5 +1,19 @@
 import * as basicTypes from './basic-types'
 import * as functionTypes from './functions'
+import houses from './houses.json'
 
 basicTypes.myRETest // /foo/
 functionTypes.getName({ first: 'Foo', last: 'Bar' }) // Foo Bar
+functionTypes.printToFile('great', console.log) //great
+functionTypes.mutate([1, 2, 3], v => v * 10) // [10,20,30]
+const outer = functionTypes.closureFunc('first')
+outer('second') // This is a closure function passed first second as arguments
+
+functionTypes.findHouses(JSON.stringify(houses))
+functionTypes.findHouses(JSON.stringify(houses), ({ name }) => name === 'Atreides')
+functionTypes.findHouses(houses)
+functionTypes.findHouses(houses, ({ name }) => name === 'Harkonnen')
+functionTypes.naturalNumbersSum(5)
+functionTypes.parseCoordinate({ x: 2, y: 2 })
+functionTypes.parseCoordinate(3, 2)
+functionTypes.parseCoordinate('x:12,y:22')
